d52ac5bee1daad34f6bdbee4401d64e5dfccf5a4
McClane Howland <mcclane.howland@gmail.com>
2016-04-02 20:34:14 -0700

    added cube class

--- Cube.java
+++ Cube.java
@@ -1,12 +1,91 @@
 import java.awt.Graphics;
 import java.awt.Color;
+import java.util.ArrayList;
 
 public class Cube {
     int x,y,z,size;
+    ArrayList<Point> points;
+    ArrayList<Polygon> polygons;
+    
     public Cube(int x,int y, int z, int size) {
-       this.x = x; 
-       this.y = y; 
-       this.z = z; 
-       this.size = size; 
+        this.x = x; 
+        this.y = y; 
+        this.z = z; 
+        this.size = size; 
+        points = new ArrayList<Point>();
+
+        points.add(new Point(x,y,z)); 
+        points.add(new Point(x+size,y,z));
+        points.add(new Point(x+size,y+size,z));
+        points.add(new Point(x,y+size,z));
+
+        points.add(new Point(x,y,z+size));
+        points.add(new Point(x+size,y,z+size));
+        points.add(new Point(x+size,y+size,z+size));
+        points.add(new Point(x,y+size,z+size));
+
+        points.add(new Point(x,y,z));
+        points.add(new Point(x,y,z+size));
+        points.add(new Point(x,y+size,z+size));
+        points.add(new Point(x,y+size,z));
+
+        points.add(new Point(x+size,y,z));
+        points.add(new Point(x+size,y,z+size));
+        points.add(new Point(x+size,y+size,z+size));
+        points.add(new Point(x+size,y+size,z));
+
+        points.add(new Point(x,y+size,z));
+        points.add(new Point(x+size,y+size,z));
+        points.add(new Point(x+size,y+size,z+size));
+        points.add(new Point(x,y+size,z+size));
+
+        points.add(new Point(x,y,z));
+        points.add(new Point(x+size,y,z));
+        points.add(new Point(x+size,y,z+size));
+        points.add(new Point(x,y,z+size));
+
+        polygons = new ArrayList<Polygon>();
+    }
+    public void draw(Graphics g) {
+        for(int i = 0; i < points.size();i += 4) {
+
+            Color color = new Color((i*100)%255,(i*100)%255,(i*100)%255);
+
+            int x1 = (int)points.get(i).x+400;
+            int x2 = (int)points.get(i+1).x+400;
+            int x3 = (int)points.get(i+2).x+400;
+            int x4 = (int)points.get(i+3).x+400;
+            int[] xpoints = {x1,x2,x3,x4};
+
+            int y1 = (int)points.get(i).y+300;
+            int y2 = (int)points.get(i+1).y+300;
+            int y3 = (int)points.get(i+2).y+300;
+            int y4 = (int)points.get(i+3).y+300;
+            int[] ypoints = {y1,y2,y3,y4};
+
+            
+            int z1 = (int)points.get(i).z;
+            int z2 = (int)points.get(i+1).z;
+            int z3 = (int)points.get(i+2).z;
+            int z4 = (int)points.get(i+3).z;
+            int[] zpoints = {z1,z2,z3,z4};
+            
+            polygons.add(new Polygon(xpoints,ypoints,zpoints,color));
+
+        }
+        //sort the polygons with largest z value first so that the closer ones are drawn over the farther
+        for(int i = 0; i < polygons.size()-1;i++) {
+            for(int j = i+1; j < polygons.size();j++) {
+                if(polygons.get(i).getLargestZ() > polygons.get(j).getLargestZ()) { //if z value is larger it will swap
+                    Polygon temp = polygons.get(i);
+                    polygons.set(i,polygons.get(j));
+                    polygons.set(j,temp);
+                }
+            }
+        }
+        for(Polygon each : polygons) {
+            each.draw(g);
+        }
+        polygons.clear();
     }
 }
--- Screen.java
+++ Screen.java
@@ -11,42 +11,13 @@ public class Screen extends JPanel {
 
     ArrayList<Point> points;
     ArrayList<Polygon> polygons;
+    ArrayList<Cube> cubes;
 
     public Screen() {
+        cubes = new ArrayList<Cube>();
         points = new ArrayList<Point>();
         polygons = new ArrayList<Polygon>();
-
-        //add four faces to the cube
-        points.add(new Point(0,0,0)); 
-        points.add(new Point(100,0,0));
-        points.add(new Point(100,100,0));
-        points.add(new Point(0,100,0));
-
-        points.add(new Point(0,0,100));
-        points.add(new Point(100,0,100));
-        points.add(new Point(100,100,100));
-        points.add(new Point(0,100,100));
-
-        points.add(new Point(0,0,0));
-        points.add(new Point(0,0,100));
-        points.add(new Point(0,100,100));
-        points.add(new Point(0,100,0));
-
-        points.add(new Point(100,0,0));
-        points.add(new Point(100,0,100));
-        points.add(new Point(100,100,100));
-        points.add(new Point(100,100,0));
-
-        points.add(new Point(0,100,0));
-        points.add(new Point(100,100,0));
-        points.add(new Point(100,100,100));
-        points.add(new Point(0,100,100));
-
-        points.add(new Point(0,0,0));
-        points.add(new Point(100,0,0));
-        points.add(new Point(100,0,100));
-        points.add(new Point(0,0,100));
-
+        cubes.add(new Cube(0,0,0,50));
     }
     public Dimension getPreferredSize() {
         return new Dimension(800,600);
@@ -62,93 +33,54 @@ public class Screen extends JPanel {
         /**
             do the rotating here:
         */
-            //rotateX(Math.PI/200); //happens every time repaint is called
+            rotateX(Math.PI/200); //happens every time repaint is called
             rotateY(Math.PI/200);
             //rotateZ(Math.PI/200);
         //axes
         gBuff.setColor(Color.black);
         gBuff.drawLine(0,300,800,300); //x
         gBuff.drawLine(400,0,400,600); //y
-        //draw the points
-        gBuff.setColor(Color.red);
-        for(Point each : points) {
-            gBuff.fillOval((int)each.x-2+400,(int)each.y-2+300,4,4);
-        }
-        //experiment with polygons
-        // creates a new polygon for each set of four points, aka a face of the cube 
-        for(int i = 0; i < points.size();i += 4) {
-
-            Color color = new Color((i*100)%255,(i*100)%255,(i*1000)%255);
-
-            int x1 = (int)points.get(i).x+400;
-            int x2 = (int)points.get(i+1).x+400;
-            int x3 = (int)points.get(i+2).x+400;
-            int x4 = (int)points.get(i+3).x+400;
-            int[] xpoints = {x1,x2,x3,x4};
-
-            int y1 = (int)points.get(i).y+300;
-            int y2 = (int)points.get(i+1).y+300;
-            int y3 = (int)points.get(i+2).y+300;
-            int y4 = (int)points.get(i+3).y+300;
-            int[] ypoints = {y1,y2,y3,y4};
-
-            
-            int z1 = (int)points.get(i).z;
-            int z2 = (int)points.get(i+1).z;
-            int z3 = (int)points.get(i+2).z;
-            int z4 = (int)points.get(i+3).z;
-            int[] zpoints = {z1,z2,z3,z4};
-            
-            polygons.add(new Polygon(xpoints,ypoints,zpoints,color));
-
-        }
-        //sort the polygons with largest z value first so that the closer ones are drawn over the farther
-        for(int i = 0; i < polygons.size()-1;i++) {
-            for(int j = i+1; j < polygons.size();j++) {
-                if(polygons.get(i).getLargestZ() > polygons.get(j).getLargestZ()) { //if z value is larger it will swap
-                    Polygon temp = polygons.get(i);
-                    polygons.set(i,polygons.get(j));
-                    polygons.set(j,temp);
-                }
-            }
-        }
-        for(Polygon each : polygons) {
+        for(Cube each : cubes) {
             each.draw(gBuff);
         }
         g.drawImage(bufferedImage,0,0,null);
-        polygons.clear();
-
     }
     
     //rotation code from tutorial
     public void rotateX(double theta) { //along the x-axis
         double cos = Math.cos(theta);
         double sin = Math.sin(theta);
-        for(Point each : points) {
-            double y = each.y;
-            double z = each.z;
-            each.y = y*cos - z*sin;
-            each.z = z*cos + y*sin;
+        for(Cube each : cubes) {
+            for(Point p : each.points) {
+                double y = p.y;
+                double z = p.z;
+                p.y = (y*cos - z*sin);
+                p.z = (z*cos + y*sin);
+            }
         }
     }
     public void rotateY(double theta) { //along the y-axis
         double cos = Math.cos(theta);
         double sin = Math.sin(theta);
-        for(Point each : points) {
-            double x = each.x;
-            double z = each.z;
-            each.x = x*cos - z*sin;
-            each.z = z*cos + x*sin;
+        for(Cube each : cubes) {
+            for(Point p: each.points) {
+                double x = p.x;
+                double z = p.z;
+                p.x = x*cos - z*sin;
+                p.z = z*cos + x*sin;
+            }
         }
     }
     public void rotateZ(double theta) { //along the z-axis
         double cos = Math.cos(theta);
         double sin = Math.sin(theta);
-        for(Point each : points) {
-            double x = each.x;
-            double y = each.y;
-            each.x = x*cos - y*sin;
-            each.y = y*cos + x*sin;
+        for(Cube each : cubes) {
+            for(Point p : each.points) {
+                double x = p.x;
+                double y = p.y;
+                p.x = x*cos - y*sin;
+                p.y = y*cos + x*sin;
+            }
         }
     }
     public void animate() {
